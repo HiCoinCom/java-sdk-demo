24dee179969e4e32faab62e1fa8c76fe308f642b
haha <18046237+zhp1254@users.noreply.github.com>
2025-11-24 19:30:45 +0800

    Update WithdrawSyncTask.java

--- custody-finance/src/main/java/com/chainup/custody/component/WithdrawSyncTask.java
+++ custody-finance/src/main/java/com/chainup/custody/component/WithdrawSyncTask.java
@@ -46,9 +46,7 @@ public class WithdrawSyncTask {
                     continue;
                 }*/
 
-                /*if(dbRow.custody_id > 0) {
-                    continue;
-                }*/
+              
 
             // 将数据写入数据库,注意 requestId 唯一；requestId在推送提现到ChainUp 时设置
             // update user_withdraw set custody_id=row.getId(), txid=row.getTxid, status=xxx, fee=row.getRealFee() where request_id=row.getRequestId()
