d0f4eb966a4c0094e866cc50eb22ffc1a76a3ad0
zhp <zhp@qq.com>
2025-11-24 18:56:47 +0800

    print custom Rsa Public key

--- custody-finance/src/main/java/com/chainup/custody/config/MpcClientConfig.java
+++ custody-finance/src/main/java/com/chainup/custody/config/MpcClientConfig.java
@@ -2,11 +2,14 @@ package com.chainup.custody.config;
 
 import com.github.hicoincom.MpcConfig;
 import com.github.hicoincom.WaasClientFactory;
+import com.github.hicoincom.crypto.rsa.RSAHelper;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import com.github.hicoincom.MpcClient;
+
+import java.security.PrivateKey;
 
 @Configuration
 public class MpcClientConfig {
@@ -28,6 +31,7 @@ public class MpcClientConfig {
 
     @Bean
     public MpcClient getClient() throws Exception {
+        //System.out.println(RSAHelper.genRSAKeys().getPrivateKey());
         if (StringUtils.isBlank(appId) || StringUtils.isBlank(customRsaPriv) || StringUtils.isBlank(chainUpRsaPub)) {
             throw new Exception("mpc client cfg error, pls check appid、 rsa private key and rsa pub key");
         }
@@ -37,6 +41,13 @@ public class MpcClientConfig {
         cfg.setEnableLog(false);
         cfg.setUserPrivateKey(customRsaPriv);
         cfg.setWaasPublickKey(chainUpRsaPub);
+
+        System.out.println("");
+        String pub = RSAHelper.getPublicKeyStringByPrivateKey(cfg.getUserPrivateKey());
+        System.out.println("请将以下Rsa公钥配置到ChainUp平台 Api管理菜单的 Client system RSA Public Key：");
+        System.out.println(pub);
+        System.out.println("");
+
         return WaasClientFactory.CreateMpcClient(cfg);
     }
 }
